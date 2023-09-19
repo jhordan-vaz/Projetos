c43cdbaa184ae7030007212481e88d51dae678eb
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-09-19 00:38:36 -0300

    Html&Css
    Primeiro projeto react.

---
+++ HTML5/teste.js
@@ -0,0 +1,11 @@
+// 1296, 4 ,4096, 6
+
+let a = 0;
+let b = 0;
+
+function resultado(a, b) {
+    expotenciacao = Math.pow(8, 4);
+    return expotenciacao;
+}
+resultado();
+console.log(resultado());
