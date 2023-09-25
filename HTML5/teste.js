2f88d2e772fa69ada3d2fa9bdbb357c2b7686e93
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-09-25 14:28:26 -0300

    Html&Css
    iniciando whatsapp clone.

--- HTML5/teste.js
+++ HTML5/teste.js
@@ -1,11 +1,49 @@
 // 1296, 4 ,4096, 6
 
-let a = 0;
-let b = 0;
+// let a = 0;
+// let b = 0;
 
-function resultado(a, b) {
-    expotenciacao = Math.pow(8, 4);
-    return expotenciacao;
+// function resultado(a, b) {
+//     expotenciacao = Math.pow(8, 4);
+//     return expotenciacao;
+// }
+// resultado();
+// console.log(resultado());
+
+
+function GetIMC (peso = 0, altura = Number, imc) {
+    this.peso = peso;
+    this.altura = altura;
+    this.imc = imc;
 }
-resultado();
-console.log(resultado());
+
+GetIMC.prototype.isValid = function() {
+    let validPeso = this.peso;
+    let validAltura = this.altura;
+    let valid = true;
+
+    
+} 
+
+
+GetIMC.prototype.CalcImc = function(alt, pes) {
+    let result;
+    this.alt = this.peso;
+    this.pes = this.altura;
+
+    
+};
+
+
+
+GetIMC.prototype.retornaTudo = function () {
+    return console.log('Olá Mundo!')
+}
+
+const imc = new GetIMC(1, 2, "IMC");
+console.log(imc);
+imc.CalcImc();
+imc.isValid();
+imc.retornaTudo();
+
+// console.log(imc.CalcImc());
