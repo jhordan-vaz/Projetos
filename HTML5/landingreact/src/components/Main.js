7c716eaffe0c6c4439700c2784188bcd630e95b1
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-09-30 19:21:32 -0300

    React
    .

--- HTML5/landingreact/src/components/Main.js
+++ HTML5/landingreact/src/components/Main.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 
+import "./Main.css";
+
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    newTask: "",
+  };
 
-    this.state = {
-      newTask: "",
-    };
-  }
-
-  inputChanged() {
-    
+  inputChanged = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
   }
 
   render() {
+    const { newTask } = this.state;
+
     return (
       <div className='main'>
         <div className='Header'>
-        <h1>Lista React</h1>
+         <h1>Treinando React</h1>
           <form action="#">
             <input onChange={this.inputChanged} type='text '/>
             <button type='submit'>Enviar</button>
