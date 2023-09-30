730e8c69e76a2f266f0c894b5a9cc1fa558dfc61
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-09-30 20:22:07 -0300

    React
    .

--- HTML5/landingreact/src/App.js
+++ HTML5/landingreact/src/App.js
@@ -3,13 +3,13 @@ import Main from "./components/Main";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Header</h1>
-      </header>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <h1>Header</h1>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
