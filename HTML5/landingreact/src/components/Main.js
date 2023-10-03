1890726c4eb1875e4c155cd3baf661b6d5b0eb4a
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-10-02 23:54:39 -0300

    React
    Problema de "key", não resolvido.

--- HTML5/landingreact/src/components/Main.js
+++ HTML5/landingreact/src/components/Main.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+//Form
+
+import { FaPlus } from "react-icons/fa";
 import "./Main.css";
 
 export default class Main extends Component {
   state = {
     newTask: "",
+    tasks: [
+      "Tarefa 1",
+      "Tarefa 2",
+      "Tarefa 3"
+    ],
   };
 
   inputChanged = (e) => {
@@ -14,17 +22,27 @@ export default class Main extends Component {
   }
 
   render() {
-    const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
-      <div className='main'>
-        <div className='Header'>
-         <h1>Treinando React</h1>
-          <form action="#">
-            <input onChange={this.inputChanged} type='text '/>
-            <button type='submit'>Enviar</button>
+      <div className="main" >
+         <h1>Learning React</h1>
+
+          <form action="#" className='form'>
+            <input onChange={this.inputChanged} type='text'/>
+            <button type='submit'>
+             <FaPlus />
+            </button>
           </form>
+
+        <div  className="ul" key={tasks}>
+         <ul className='tasks-ul'>
+           {tasks.map(t => (
+            <li>{tasks}</li>
+           ))}
+         </ul>
         </div>
+
       </div>
     );
   }
