788c5a320f6c3fc98a45a4da2672033886df2d8c
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-10-04 23:57:59 -0300

    React Vite
    .

--- HTML5/todoList/src/App.jsx
+++ HTML5/todoList/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
 
 function App() {
-  const[all, setAll] = useState()
+  const [all, setAll] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar programação",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
 
   return  <div>Hello World!</div>
-  
 }
 
 export default App
