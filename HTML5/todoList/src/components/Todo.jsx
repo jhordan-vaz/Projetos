e3faa25039d5b327f3e123c54bb5af05ce52c4e1
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-10-09 01:00:54 -0300

    React Vite
    Correção Botoes Asc e Desc

--- HTML5/todoList/src/components/Todo.jsx
+++ HTML5/todoList/src/components/Todo.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div 
+     className="todo"
+     style={{ textDecoration: todo.isCompleted ? "line-through" : ""}}
+    >
     <div className="content">
-      <p>{todo.text}</p>{todo.text}
+      <p>{todo.text}</p>
       <p className="category">({todo.category})</p>
     </div>
     <div>
-      <button>Completo</button>
-      <button>x</button>
+      <button className='complete' onClick={() => completeTodo(todo.id)}>Completo</button>
+      <button className='remove' onClick={() => removeTodo(todo.id)}>x</button>
     </div>
    </div>
   )
 }
 
 export default Todo;
+
+
